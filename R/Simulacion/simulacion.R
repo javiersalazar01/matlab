9bf26413595f6a649f5ed14d13d5dcd788db946d
Javier Salazar <javi_9315@hotmail.com>
2015-11-24 20:31:46 -0700

    Commit con graficas

--- R/Simulacion/simulacion.R
+++ R/Simulacion/simulacion.R
@@ -31,7 +31,7 @@ Probabilidad <- function(Matrix_Casos, Columna, valor_col){
   
 }
 
-##Retorna una matris boleana donde se representa los registros con o sin cancer, dada los casos de prueba dados.
+##Retorna una matriZ boleana donde se representa los registros con o sin cancer, dada los casos de prueba dados.
 prediccion <- function(Test, Conteo_p_norm, Prob_p, Conteo_n_norm, Prob_n){
   
   Cols <- c(1:5);
@@ -52,10 +52,8 @@ prediccion <- function(Test, Conteo_p_norm, Prob_p, Conteo_n_norm, Prob_n){
       
     }
     
-    
     ProbsP = ProbsP * Prob_p;
     ProbsN = ProbsN * Prob_n;
-    
     
     if (ProbsP > ProbsN) {
       Prediccion[num_reg, 1] = 1;
@@ -68,9 +66,18 @@ prediccion <- function(Test, Conteo_p_norm, Prob_p, Conteo_n_norm, Prob_n){
 }
 
 
-
-
-
+#funcion pastelito la cual me grafica una grafica de pastel muy bonita de los datos espesificados en el arreglo info y title es el titulo#
+pastelito <- function(info,gruopNames,title){
+  
+  suma <- sum(info);
+  persents <- formatC((info[]/suma)*100,digits=2, format="f");
+  gruopNames <- paste(gruopNames, persents); 
+  gruopNames <- paste(gruopNames,"%",sep="");
+  x11();
+  piePosi <- pie(info, col=rainbow(length(gruopNames)) ,labels = gruopNames, main=title);
+  
+}
+##pastelito function end
 dimRaw <- dim(raw);
 
 raw_rows_count <- dimRaw[1];
@@ -87,7 +94,6 @@ idxTest <- setdiff(1:raw_rows_count,idx);
 test <- raw[idxTest,]
 #train: contiene los elementos que se utilizaran como entrenamiento para nuestro clasificador".
 #test: contiene los datos de prueba paa calar si funciona bien, son todos diferentes a los registros de train."
-
 
 
 uniq <- data.matrix(unique(unlist(raw)));
@@ -123,8 +129,6 @@ conteo_n <- Counting(train_n, Cols, uniq );
 colSums(conteo_n);
 #obtener conteo de cada ocurrencia.
 
-
-
 #============================================
 conteo_p2 <- cbind(conteo_p[,1],conteo_p[,2:6] + 1);
 conteo_p_norm <- cbind(conteo_p2[,1],conteo_p2[,2:6]/Num_Posi);
@@ -144,17 +148,24 @@ test_n <- test[conteo,];
 PredictionP = prediccion(test_p, conteo_p_norm, prob_p, conteo_n_norm, prob_n);
 PredictionN = prediccion(test_n, conteo_p_norm, prob_p, conteo_n_norm, prob_n);
 
+## fin del algoritmooo, despues de aqui comienza el analasisi de los datos
 positivos_en_positivos <- sum(PredictionP[,1] == 1);
 negativos_en_positivos <- sum(PredictionP[,1] == 0);
 
 negativos_en_negativos <- sum(PredictionN[,1] == 0);
 positivos_en_negativos <- sum(PredictionN[,1] == 1);
 
-
 aciertoPosi = positivos_en_positivos/nrow(test_p);
 aciertoNega = negativos_en_negativos/nrow(test_n);
 
 
+#porsentage de aciertos positivos y aciertos negativos.
+paste("aciertos Positivos: ", aciertoPosi);
+paste("aciertos negativos ",aciertoNega);
+
+
+
+#Elaboracion de la matriz de confucion.
 matriz_con <- matrix(0,3,2);
 
 matriz_con[1,1] = positivos_en_positivos;
@@ -167,14 +178,32 @@ matriz_con[3,2] = negativos_en_negativos + positivos_en_negativos;
 
 colnames(matriz_con) <- c("P","N");
 rownames(matriz_con) <- c("P","N","Tot");
+colnames(raw) <- c("BID-RADS","Edad","Forma","Margen","Densidad","Gravedad");
 
 matriz_con
-
-paste("aciertos Positivos: ", aciertoPosi);
-paste("aciertos negativos ",aciertoNega);
+##Fin de elaboracion de matriz de confucion.
 
 
-x11()
-pairs(raw[,1:5], col=raw[,6])
+##grafica de pastel positivos y negativos
+info <- c(positivos_en_positivos,negativos_en_positivos);
+pastelito(info,c("Aciertos Positivos","Falsos Negativos"),"Positivos Test Resultado");
+
+info <- c(negativos_en_negativos,positivos_en_negativos);
+pastelito(info,c("Aciertos Negativos","Falsos Positivos"),"Negativos Test Resultado");
+##fin de graficas de pastel.
+
+
+##inicio de codigo para imprimir matriz de graficas de disporcion###.
+cols <- character(nrow(raw));
+cols[] <- "black";
+
+cols[raw$Gravedad == 0] <- "blue";
+cols[raw$Gravedad == 1] <- "red";
+x11();
+pairs(raw[,1:5], col=cols,main="Matriz de graficas de dispersión.", pch = 21,oma=c(4,4,6,12));
+par(xpd=TRUE)
+legend(0.88, 0.7, c("Benigno","Maligno"),  
+       fill=c("blue", "red"));
+###end matriz de graficas de dispercion###.
 
 
